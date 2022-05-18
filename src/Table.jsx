fcd5e5f7b4c010c1b0743b319e2a7ebbeb7820de
joseph.stallone@dreamorbit.com <joseph.stallone@dreamorbit.com>
2022-05-18 20:57:08 +0530

    first commit

---
+++ src/Table.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+const Table = ({data}) => {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                <th>Id</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Email</th>
+                    
+                </tr>
+                {data.map((item) => (
+                    <tr key = {item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.first_name}</td>
+                    <td>{item.last_name}</td>
+                    <td>{item.email}</td>
+                </tr>
+                ))}
+                
+            </tbody>
+        </table>
+    )
+}
+
+
+
+export default Table
